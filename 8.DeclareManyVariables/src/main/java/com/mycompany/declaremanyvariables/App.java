0312377bb074950aae5f7c0a3a701d2b14f9cd02
Ndamulelo30 <141257791+Ndamulelo30@users.noreply.github.com>
2024-02-16 14:07:26 +0200

    Add files via upload

---
+++ 8.DeclareManyVariables/src/main/java/com/mycompany/declaremanyvariables/App.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.declaremanyvariables;
+
+/**
+ *
+ * @author Sanele
+ */
+public class App {
+
+    public static void main(String[] args) {
+        int x = 7,y = 8, z = 77;
+        System.out.println(x + y + z);
+        
+        
+    }
+}
